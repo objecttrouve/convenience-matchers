de13a6515cb079e435e66cb2c4627e833b6ee9b4
objecttrouve <un.object.trouve@gmail.com>
2023-06-19 13:06:07 +0200

    Add test for map matcher descriptions

--- src/test/java/org/objecttrouve/testing/matchers/fluentits/FluentMapMatcherTest.java
+++ src/test/java/org/objecttrouve/testing/matchers/fluentits/FluentMapMatcherTest.java
@@ -8,6 +8,7 @@ import java.util.TreeMap;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
+import org.hamcrest.StringDescription;
 import org.junit.Test;
 import static org.objecttrouve.testing.matchers.ConvenientMatchers.aMapLike;
 
@@ -96,5 +97,92 @@ public class FluentMapMatcherTest {
         assertThat(map, is(aMapLike(map).sorted(comparator)));
     }
 
+    @Test
+    public void describeMismatchSafelyKeyValMismatch() {
+        final Map<Integer, String> map = new HashMap<>();
+        map.put(1, "1");
+        map.put(2, "2");
 
+        String expectedDescription = "\n" +
+                "Findings:\n" +
+                "\"Not all expectations were fulfilled.\"\n" +
+                "\n" +
+                "⦗0⦘⦗1=1⦘           \uD83D\uDC94⦗0⦘⦗<{k=2, v=1}>⦘ \uD83D\uDC94⦗1⦘⦗<{k=1, v=2}>⦘\n" +
+                "⦗1⦘⦗2=2⦘           \uD83D\uDC94⦗1⦘⦗<{k=1, v=2}>⦘ \uD83D\uDC94⦗0⦘⦗<{k=2, v=1}>⦘\n\n";
+
+        FluentMapMatcher<Integer, String> matcher = aMapLike(map)
+                .withKeyVal(2, "1")
+                .withKeyVal(1, "2");
+
+        checkMismatchDescription(map, matcher, expectedDescription);
+    }
+
+    @Test
+    public void describeMismatchSafelyKeyValMatcherMismatch(){
+        final Map<Integer, String> map = new HashMap<>();
+        map.put(1, "1");
+        map.put(2, "2");
+
+        //noinspection ConcatenationWithEmptyString
+        String expectedDescription = "" +
+                "\nFindings:\n\"" +
+                "Not all expectations were fulfilled.\"\n\n" +
+                "⦗0⦘⦗1=1⦘           💔⦗0⦘⦗key ⩳ '<3>'; value ⩳ '\"1\"'⦘ 💔⦗1⦘⦗key ⩳ '<2>'; value ⩳ '\"2\"'⦘\n" +
+                "⦗1⦘⦗2=2⦘💕        \n\n";
+
+        FluentMapMatcher<Integer, String> matcher = aMapLike(map)
+                .withKeyValMatching(equalTo(3), equalTo("1"))
+                .withKeyValMatching(equalTo(2), equalTo("2"));
+
+        checkMismatchDescription(map, matcher, expectedDescription);
+    }
+
+    @Test
+    public void describeMismatchSafelySortOrder(){
+        final Map<Integer, String> map = new LinkedHashMap<>();
+        map.put(3, "3");
+        map.put(2, "2");
+        map.put(1, "1");
+
+        //noinspection ConcatenationWithEmptyString
+        String expectedDescription = "" +
+                "\nFindings:\n" +
+                "\"Collection is not sorted.\"\n\n" +
+                "⦗0⦘⦗3=3⦘          \n" +
+                "⦗1⦘⦗2=2⦘  ↕       \n" +
+                "⦗2⦘⦗1=1⦘  ↕       \n\n";
+
+        FluentMapMatcher<Integer, String> matcher = aMapLike(map).sorted();
+
+        checkMismatchDescription(map, matcher, expectedDescription);
+    }
+
+    @Test
+    public void describeMismatchSafelySortOrderWithComparator(){
+        Comparator<Integer> comparator = (i1, i2) -> i2 - i1;
+        final Map<Integer, String> map = new TreeMap<>();
+        map.put(2, "2");
+        map.put(3, "3");
+        map.put(1, "1");
+
+        //noinspection ConcatenationWithEmptyString
+        String expectedDescription = "" +
+                "\nFindings:\n" +
+                "\"Collection is not sorted.\"\n\n" +
+                "⦗0⦘⦗1=1⦘          \n" +
+                "⦗1⦘⦗2=2⦘  ↕       \n" +
+                "⦗2⦘⦗3=3⦘  ↕       \n\n";
+
+        FluentMapMatcher<Integer, String> matcher = aMapLike(map).sorted(comparator);
+
+        checkMismatchDescription(map, matcher, expectedDescription);
+    }
+
+    private static void checkMismatchDescription(Map<Integer, String> map, FluentMapMatcher<Integer, String> matcher, String expectedDescription) {
+        boolean matches = matcher.matchesSafely(map);
+        assertThat(matches, is(false));
+        final StringDescription description = new StringDescription();
+        matcher.describeMismatchSafely(map, description);
+        assertThat(description.toString(), is(expectedDescription));
+    }
 }
