38ece772369a4c7c0289d5f2c21bdb1e1f96b77a
objecttrouve <un.object.trouve@gmail.com>
2024-07-03 14:58:22 +0200

    Add `MatchMatrix`
    Introduced a new class that will encapsulate the more complex operations on the match matrix array.
    (To be added in a later commit.)

---
+++ src/main/java/org/objecttrouve/testing/matchers/fluentits/MatchMatrix.java
@@ -0,0 +1,31 @@
+package org.objecttrouve.testing.matchers.fluentits;
+
+class MatchMatrix {
+
+    private final double[][] matchMatrix;
+
+    MatchMatrix(final int nrOfExpectations, final int nrOfActualItems) {
+        matchMatrix = new double[nrOfExpectations][nrOfActualItems];
+    }
+
+    void match(final int expectationIndex, final int actualItemIndex) {
+        matchMatrix[expectationIndex][actualItemIndex] = 1.0;
+    }
+
+    void mismatch(final int expectationIndex, final int actualItemIndex) {
+        matchMatrix[expectationIndex][actualItemIndex] = 0.0;
+    }
+
+    void scoredMismatch(final int expectationIndex, final int actualItemIndex, double score) {
+        matchMatrix[expectationIndex][actualItemIndex] = score;
+    }
+
+    boolean matched(final int expectationIndex, final int actualItemIndex) {
+        return matchMatrix[expectationIndex][actualItemIndex] == 1.0;
+    }
+
+    double getScore(final int expectationIndex, final int actualItemIndex) {
+        return matchMatrix[expectationIndex][actualItemIndex];
+    }
+
+}
---
+++ src/test/java/org/objecttrouve/testing/matchers/fluentits/MatchMatrixTest.java
@@ -0,0 +1,95 @@
+package org.objecttrouve.testing.matchers.fluentits;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class MatchMatrixTest {
+
+
+
+
+    @Test
+    public void test__MatchMatrix__match__matched(){
+
+        final MatchMatrix matchMatrix = new MatchMatrix(2, 2);
+
+        matchMatrix.match(0, 1);
+
+        assertTrue(matchMatrix.matched(0,1));
+        assertFalse(matchMatrix.matched(0,0));
+        assertFalse(matchMatrix.matched(1,0));
+        assertFalse(matchMatrix.matched(1,1));
+    }
+
+    @Test
+    public void test__MatchMatrix__mismatch__matched(){
+
+        final MatchMatrix matchMatrix = new MatchMatrix(2, 2);
+
+        matchMatrix.mismatch(0, 1);
+
+        assertFalse(matchMatrix.matched(0,1));
+        assertFalse(matchMatrix.matched(0,0));
+        assertFalse(matchMatrix.matched(1,0));
+        assertFalse(matchMatrix.matched(1,1));
+    }
+
+    @Test
+    public void test__MatchMatrix__match__getScore(){
+
+        final MatchMatrix matchMatrix = new MatchMatrix(2, 2);
+
+        matchMatrix.match(0, 1);
+
+        assertThat(matchMatrix.getScore(0,1), is(1.0));
+        assertThat(matchMatrix.getScore(0,0), is(0.0));
+        assertThat(matchMatrix.getScore(1,0), is(0.0));
+        assertThat(matchMatrix.getScore(1,1), is(0.0));
+    }
+
+    @Test
+    public void test__MatchMatrix__mismatch__getScore(){
+
+        final MatchMatrix matchMatrix = new MatchMatrix(2, 2);
+
+        matchMatrix.mismatch(0, 1);
+
+        assertThat(matchMatrix.getScore(0,1), is(0.0));
+        assertThat(matchMatrix.getScore(0,0), is(0.0));
+        assertThat(matchMatrix.getScore(1,0), is(0.0));
+        assertThat(matchMatrix.getScore(1,1), is(0.0));
+    }
+
+    @Test
+    public void test__MatchMatrix__scoredMismatch__getScore(){
+
+        final MatchMatrix matchMatrix = new MatchMatrix(2, 2);
+
+        matchMatrix.scoredMismatch(0, 1, 0.5);
+
+        assertThat(matchMatrix.getScore(0,1), is(0.5));
+        assertThat(matchMatrix.getScore(0,0), is(0.0));
+        assertThat(matchMatrix.getScore(1,0), is(0.0));
+        assertThat(matchMatrix.getScore(1,1), is(0.0));
+    }
+
+    @Test
+    public void test__MatchMatrix__scoredMismatch_mismatch_match_getScore(){
+
+        final MatchMatrix matchMatrix = new MatchMatrix(2, 2);
+
+        matchMatrix.scoredMismatch(0, 1, 0.5);
+        matchMatrix.match(0, 0);
+        matchMatrix.mismatch(1, 1);
+
+        assertThat(matchMatrix.getScore(0,1), is(0.5));
+        assertThat(matchMatrix.getScore(0,0), is(1.0));
+        assertThat(matchMatrix.getScore(1,0), is(0.0));
+        assertThat(matchMatrix.getScore(1,1), is(0.0));
+    }
+
+
+}
